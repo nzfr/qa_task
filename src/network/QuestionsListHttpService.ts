8237c216396f9becc87b2a873f9f08228f5bcc84
nzfr <amir.nazarifar.75@gmail.com>
2022-11-15 23:50:48 +0330

    feat: get question details api

--- src/network/QuestionsListHttpService.ts
+++ src/network/QuestionsListHttpService.ts
@@ -9,12 +9,34 @@ export interface QuestionDTO {
     createdTime: string,
     createdDate: string,
     userName: string,
-    "avatar": string,
+    avatar: string,
     images: string[],
-    "answers": []
+    answers: AnswerDTO[]
+}
+
+export interface AnswerDTO {
+    id: string,
+    questionId: string,
+    userName: string,
+    userAvatar: string,
+    createdTime: string,
+    createdDate: string,
+    likesCount: string,
+    dislikesCount: string,
+    content:string
 }
 
 export const getQuestions = async (): Promise<QuestionDTO[]> => {
-    const data = await axios.get<QuestionDTO[]>('/questions');
+    const data = await axios.get<QuestionDTO[]>('/questions?_embed=answers');
+    return data.data;
+}
+
+export type GetQuestionParams = {
+    questionId: string,
+    abortController: AbortController
+}
+
+export const getQuestion = async (params:GetQuestionParams): Promise<QuestionDTO> => {
+    const data = await axios.get<QuestionDTO>(`/questions/${params.questionId}?_embed=answers`,{signal:params.abortController.signal})
     return data.data;
 }
